3c049dffb4c9e9f9261bc1c6bae3f4e78408f7a5
Mayumi Kuniyoshi <cinthya.m.kuniyoshi@gmail.com>
2024-08-16 21:30:37 -0300

    Create switchcase.java

---
+++ src/exercises/switchcase.java
@@ -0,0 +1,30 @@
+package exercises;
+
+import java.util.Scanner;
+
+public class switchcase {
+	public static void main(String args[]) {
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Digite:");
+		
+		int num = sc.nextInt();
+		
+		switch (num) {
+		
+		case 1:
+			System.out.println("1");
+			break;
+			
+		case 2:
+			System.out.println("2");
+			break;
+			
+	
+			
+		default:
+			System.out.println("errouuu");
+			
+		}
+	}
+}
